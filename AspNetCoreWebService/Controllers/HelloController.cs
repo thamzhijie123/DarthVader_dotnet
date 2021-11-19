53f49135118bccce71a4cfd1aa2c58b01da164d8
thamzhijie123 <94600011+thamzhijie123@users.noreply.github.com>
2021-11-19 20:48:46 +0800

    Update HelloController.cs

--- AspNetCoreWebService/Controllers/HelloController.cs
+++ AspNetCoreWebService/Controllers/HelloController.cs
@@ -28,7 +28,7 @@ namespace AspNetCoreWebService.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
-            Assert.Equal("Hello World!!!", response.output);
+            Assert.Equal("Hello World", response.output);
         }        
     }
 }
